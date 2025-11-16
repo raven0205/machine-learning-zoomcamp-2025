1fdcccd2d7b4ac76610d8001544501136ac3860b
Teo Hui Qian <73115813+raven0205@users.noreply.github.com>
2025-11-16 07:53:41 +0000

    added test.py

---
+++ 000-midterm-project/test.py
@@ -0,0 +1,17 @@
+# 000-midterm-project/test.py
+
+import requests
+url = "http://localhost:8000/predict"
+
+client = {
+    "month": "2024-01",
+    "town": "ANG MO KIO",
+    "flat_type": "3 ROOM",
+    "block": "308B",
+    "street_name": "ANG MO KIO AVE 1",
+    "storey_range": "01 TO 03",
+    "floor_area_sqm": 70,
+    "lease_commence_date": 2012
+}
+response = requests.post(url, json=client).json()
+print(response)
